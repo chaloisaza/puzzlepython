c69db1fb3058dc8dbb5394c507e256706d321ebc
chaloisaza <chaloisaza@gmail.com>
2018-08-29 20:51:21 -0500

    Inicio

---
+++ busqueda.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 29 19:26:28 2018
+
+@author: chaloisaza
+"""
+
+import random
+import numpy
+
+ # return an int, one of 1,2,3,4,5,6
+
+def createInitialParams(n,m):
+    list = []
+    while True:
+        r = random.randrange(0,9)      
+        if r not in list: 
+            list.append(r)
+        
+        if len(list) == 9:
+            x = reshape(list,(n,m))
+            return(x)
+            break
+        
+def createSolution(n,m):
+
+    solution = []
+    solution.extend(range(1, (n*m)+1))
+    solution = reshape(solution,(n,m))
+    return solution
+        
+ 
+def main():
+    
+  estado_inicial =  createInitialParams(3,3)
+  print (estado_inicial)
+  #solucion = createSolution(3,3)
+  solucion = [1 ,2 ,3,4 ,5 ,6,7, 8, 0]
+  #print (reshape(solucion,(3,3)))
+  
+  print(numpy.where(estado_inicial==0))
+
+if __name__ == '__main__':
+    main()
