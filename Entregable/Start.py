c4dd94fcdb26e4b5fb5223613d04855615b13cc0
chaloisaza <chaloisaza@gmail.com>
2018-09-02 22:53:22 -0500

    Fixes

--- Entregable/Start.py
+++ Entregable/Start.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from random import shuffle
 from Solve import Solve
-
 
 # creaar un array entre 0 y 8 y hacerlo random
 puzzle = range(9)
 shuffle(puzzle)
-print('Arreglo a resolver: ', puzzle)
+print("Arreglo a resolver: ",puzzle)
 # enviar ese array a clase Solve
 solution = Solve(puzzle)
 
